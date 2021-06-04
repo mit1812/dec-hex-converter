53eb1834b0a8d72a0ee40b496131e62dd15c13b5
Mitesh Patel <mit06@hotmail.com>
2021-06-04 22:06:49 +0100

    update port details

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'development') {
 const swaggerDocument = YAML.load('./swagger.yml');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/api/convert/:value', decimalToHexConverter);
